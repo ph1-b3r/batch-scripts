4c0097482a85ab023110f6343ca73dfafa5fd714
Tanveer Ali <46398857+t4nny@users.noreply.github.com>
2020-02-07 14:39:17 +0530

    Add files via upload

---
+++ pc_info.bat
@@ -0,0 +1,24 @@
+@ echo off
+
+title GetInfo(); By Tanny!&cls&
+echo ============================================================================&
+echo  #Get_Info(); #&
+
+echo # By Tanny#&
+echo ============================================================================&
+
+
+color 9 
+wmic baseboard list brief
+wmic cpu get name
+wmic MEMORYCHIP get BankLabel, DeviceLocator, Capacity, Speed
+wmic os get Caption
+wmic diskdrive get model,size
+wmic logicaldisk get name,size
+wmic sounddev get Caption
+wmic path win32_VideoController get name
+wmic nic get caption
+
+REM wmic product get name
+
+PAUSE
